e8fc93a0ef5fe633d3bbae8cd976f128a6b7d2e6
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-04-20 22:12:28 +0300

    init: add ESLint

---
+++ src/component/layer/Layer.js
@@ -0,0 +1,13 @@
+import './_layer.scss';
+
+export default class Layer {
+  get SELECTOR() {
+    return {
+      header: 'header',
+      main: 'main',
+      footer: 'footer',
+    };
+  }
+  // constructor() {};
+  init() {}
+}
