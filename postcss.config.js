9041a43e6690109e408c67eabbae6b006006a0c4
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-04-21 11:47:04 +0300

    init: add webpack

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+module.exports = {
+  // Add you postcss configuration here
+  // Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
+  plugins: [["autoprefixer"]],
+};
