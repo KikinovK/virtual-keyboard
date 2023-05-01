bf81142524bc7e95cb195bb74244b6a51603d857
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-05-01 17:04:05 +0300

    feat: add footer

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -6,5 +6,7 @@ export default class Footer extends CreateElement {
     super(paternElement, 'footer', 'footer');
 
     this.sectionIn = new CreateElement(this.element, 'div', 'section_in');
+    this.title = new CreateElement(this.sectionIn.element, 'p', 'footer__note', 'клавиатура создавалась в windows');
+    this.title = new CreateElement(this.sectionIn.element, 'p', 'footer__note', 'смена раскладки клавиатуры левый ctr+alt');
   }
 }
